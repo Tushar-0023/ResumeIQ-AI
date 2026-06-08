68680a19ae0f5ea54520cd0b93b34e41c8dc94ec
Tushar Rajput <tusharsingh2323@gmail.com>
2026-06-08 19:22:58 +0530

    Refactor API service by removing token interceptor
    Removed automatic token attachment in request interceptor and added a console log for the API URL.

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
+console.log("API URL =", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-});
-
-// Attach token automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
 });
 
 export default API;
